30d39167b159342bf141925f8a4bab3ed7e17853
flux <49762827+0xflux@users.noreply.github.com>
2025-12-16 19:21:21 +0000

    rm blocker on exports

--- implant/build.rs
+++ implant/build.rs
@@ -36,12 +36,12 @@ fn write_exports_to_build_dir() {
     let dest = out_dir.join("custom_exports.rs");
     let mut code = String::new();
 
-    let stage = env::var("STAGE_TYPE").unwrap_or_default();
+    // let stage = env::var("STAGE_TYPE").unwrap_or_default();
     // If we are not a DLL, write an empty file so the compiler is happy
-    if stage.to_lowercase() != "dll" {
-        fs::write(dest, String::new()).unwrap();
-        return;
-    };
+    // if stage.to_lowercase() != "dll" {
+    //     fs::write(dest, String::new()).unwrap();
+    //     return;
+    // };
 
     let exports_usr_machine_code = env::var("EXPORTS_USR_MACHINE_CODE").ok();
     let exports_proxy = env::var("EXPORTS_PROXY").ok();
