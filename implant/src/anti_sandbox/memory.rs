c1c0db6872e6f10170e06c3aa3f94be28df1d403
flux <49762827+0xflux@users.noreply.github.com>
2026-03-15 10:14:54 +0000

    Print dbg error before hard panic

--- implant/src/anti_sandbox/memory.rs
+++ implant/src/anti_sandbox/memory.rs
@@ -6,13 +6,16 @@ const MIN_ACCEPTABLE_MEMORY: u64 = 4000000; // ~ 4 GB
 
 /// Checks the installed amount of memory, and panics if it's less than [`MIN_ACCEPTABLE_MEMORY`]
 /// or if the WinAPI call failed.
+#[allow(unreachable_code)]
 pub fn validate_ram_sz_or_panic() {
     let mut total_memory: u64 = 0;
 
     if unsafe { GetPhysicallyInstalledSystemMemory(&mut total_memory) } == FALSE {
         #[cfg(debug_assertions)]
         {
-            panic!("GetPhysicallyInstalledSystemMemory error")
+            use crate::dbgprint;
+
+            dbgprint!("GetPhysicallyInstalledSystemMemory error")
         }
 
         panic!()
@@ -21,7 +24,9 @@ pub fn validate_ram_sz_or_panic() {
     if total_memory < MIN_ACCEPTABLE_MEMORY {
         #[cfg(debug_assertions)]
         {
-            panic!("Total memory ({total_memory}) was less than {MIN_ACCEPTABLE_MEMORY}")
+            use crate::dbgprint;
+
+            dbgprint!("Total memory ({total_memory}) was less than {MIN_ACCEPTABLE_MEMORY}")
         }
 
         panic!()
